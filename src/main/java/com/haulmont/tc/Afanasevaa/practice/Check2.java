0e94804ee9dd193bcbc31eb6f866f26922462115
Afanasevaa36 <a.afanasev@haulmont.com>
2022-05-17 07:53:18 +0300

    Выполнил пункт 1.14

---
+++ src/main/java/com/haulmont/tc/Afanasevaa/practice/Check2.java
@@ -0,0 +1,16 @@
+package com.haulmont.tc.Afanasevaa.practice;
+
+public class Check2 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("Arkadiy");
+        checkAnotherPerson(person);
+        System.out.println(person.getFirstName());
+    }
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
+        System.out.println(person.getFirstName());
+    }
+
+}
